7a14b9b9c5cfe3eafcafa8f7a717665e1590bf18
John Reese <john@noswap.com>
2014-07-26 23:37:38 -0700

    More debug, and heartbeat

--- client.js
+++ client.js
@@ -1,12 +1,26 @@
 $(document).ready(function() {
-	var websocket = new WebSocket("ws://localhost:9000/");
-
-	websocket.onmessage = function(event) {
-		console.log("Message received: " + event.data);
-	}
+	var websocket = new WebSocket("ws://localhost:9000");
 
 	websocket.onopen = function(event) {
 		console.log("Connection open: " + event.data);
 		websocket.send("test message");
 	}
+
+	websocket.onclose = function(event) {
+		console.log("Connection closed");
+	}
+
+	websocket.onerror = function(event) {
+		console.error("Error: " + event.data);
+	}
+
+	websocket.onmessage = function(event) {
+		console.log("Message received: " + event.data);
+	}
+
+	var heartbeat = function() {
+		websocket.send("heartbeat");
+	}
+	setInterval(heartbeat, 5000);
+
 });

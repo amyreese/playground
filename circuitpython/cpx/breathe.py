b3033910f9282c65054961b814bb036f9bf581f7
John Reese <john@noswap.com>
2019-02-27 17:42:25 -0800

    blue/yellow breathing

--- circuitpython/cpx/breathe.py
+++ circuitpython/cpx/breathe.py
@@ -5,18 +5,19 @@ from colors import RAINBOW, BLUE, YELLOW, WHITE
 from cpgame import every, tick, start
 
 STEP = 1.0 / 256
-UPPER = STEP * 10
-LOWER = STEP
+UPPER = STEP * 30
+LOWER = STEP * 2
 START = LOWER
 
 pixels = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness=START)
-pixels.fill(WHITE)
+pixels.fill(BLUE)
+pixels[3], pixels[4], pixels[8], pixels[9] = YELLOW, YELLOW, YELLOW, YELLOW
 
 class state:
     value = START
     step = STEP
 
-@every(0.15)
+@every(0.05)
 def main(now):
     if pixels.brightness >= UPPER and state.step > 0:
         state.step = -state.step

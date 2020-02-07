4e7f8aa84320a460ddf86ef726e826e1400dd2ec
John Reese <john@noswap.com>
2020-02-06 17:59:39 -0800

    Poking at gamepad

--- circuitpython/pygamer/parsec.py
+++ circuitpython/pygamer/parsec.py
@@ -1,19 +1,35 @@
 import board
 import neopixel
+import random
 
 from colors import RED, GREEN, BLUE
 
-from cpgame import start, after, every, on, DIGITALIO, TOUCHIO
+from cpgame import (
+    start, after, every, on, sample, enable_speaker, play_sound,
+    DIGITALIO, TOUCHIO, GAMEPAD
+)
+
+SOUNDS = {
+    "blue": sample(659),  # E
+    "green": sample(329),  # Low E
+    "red": sample(440),  # A
+    "yellow": sample(554),  # C#
+}
+NAMES = list(SOUNDS.keys())
 
 pixels = neopixel.NeoPixel(board.NEOPIXEL, 5, brightness=0.05, auto_write=False)
 pixels.fill(BLUE)
 pixels.show()
 
-@every(1)
+@every(7)
 def loop(now):
-    print(now)
+    name = random.choice(NAMES)
+    sound = SOUNDS[name]
+    play_sound(sound, 0.1)
+    print(name)
 
 for pin in TOUCHIO:
+    break
     try:
         @on(pin)
         def pressed(now):
@@ -22,4 +38,10 @@ for pin in TOUCHIO:
     except Exception as e:
         print(e)
 
+@on(GAMEPAD.A)
+def pressed(now):
+    print("A pressed")
+
+
+# enable_speaker()
 start()
